56211138e1297622ce902142f32f7bc5b714d001
SenYeezus <simpsonsenyo@gmail.com>
2022-07-10 10:11:08 +0200

    core functionality in macro working

--- chrono-macros/src/lib.rs
+++ chrono-macros/src/lib.rs
@@ -1,9 +1,16 @@
 use proc_macro::TokenStream;
 
 mod alloc;
+mod main;
 
 #[proc_macro_attribute]
 pub fn alloc(_: TokenStream, item: TokenStream) -> TokenStream {
     let f: syn::ItemFn = syn::parse(item).expect("Could not parse input tokenstream");
     alloc::alloc(f)
+}
+
+#[proc_macro_attribute]
+pub fn main(_: TokenStream, item: TokenStream) -> TokenStream {
+    let f: syn::ItemFn = syn::parse(item).expect("Could not parse input tokenstream");
+    main::main(f)
 }
