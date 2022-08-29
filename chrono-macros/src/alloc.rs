ec8fea09cba943f54f3757c86600d6b7b542a748
SenYeezus <simpsonsenyo@gmail.com>
2022-08-29 20:23:39 +0200

    alloc macro works with new nightly

--- chrono-macros/src/alloc.rs
+++ chrono-macros/src/alloc.rs
@@ -2,33 +2,36 @@ use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::ReturnType;
 
-pub(super) fn alloc(mut f: syn::ItemFn) -> TokenStream {
-    let mut arg_names: syn::punctuated::Punctuated<syn::Ident, syn::Token![,]> =
-        syn::punctuated::Punctuated::new();
+pub(super) fn alloc(f: syn::ItemFn) -> TokenStream {
+    let mut arg_names = Vec::new();
     let mut fn_args = f.sig.inputs.clone();
 
     for arg in fn_args.iter_mut() {
         match arg {
             syn::FnArg::Receiver(_) => {}
             syn::FnArg::Typed(t) => {
-                if let syn::Pat::Ident(i) = t.pat.as_mut() {
-                    arg_names.push(i.ident.clone());
-                    i.mutability = None;
+                if let syn::Pat::Ident(id) = t.pat.as_mut() {
+                    arg_names.push(id.ident.clone());
+                    id.mutability = None;
                 }
             }
         }
     }
 
+    let inputs = f.sig.inputs.clone();
+    let output = f.sig.output.clone();
+
     let fn_name = f.sig.ident.clone();
+    let inner_fn_name = format_ident!("__{}_task", fn_name);
+    let mut inner_fn = f;
 
-    let fn_args = f.sig.inputs.clone();
-    let visibility = &f.vis;
-    let attrs = &f.attrs;
-
-    f.sig.ident = format_ident!("task");
+    let fn_args = inputs;
+    let visibility = inner_fn.vis.clone();
+    inner_fn.vis = syn::Visibility::Inherited;
+    inner_fn.sig.ident = inner_fn_name.clone();
 
     let impl_ty = {
-        match f.sig.output.clone() {
+        match output.clone() {
             ReturnType::Default => {
                 quote!(impl ::core::future::Future<Output = ()>)
             }
@@ -39,42 +42,41 @@ pub(super) fn alloc(mut f: syn::ItemFn) -> TokenStream {
     };
 
     let fn_ret = {
-        match f.sig.output.clone() {
+        match output.clone() {
             ReturnType::Default => {
-                // quote!(::chrono::task::RawTask<#impl_ty, (), *mut ::chrono::runtime::queue::Queue>)
                 quote!(::chrono::task::RawTask<#impl_ty, ()>)
             }
             ReturnType::Type(_, ret) => {
-                // quote!(::chrono::task::RawTask<#impl_ty, #ret, *mut ::chrono::runtime::queue::Queue>)
                 quote!(::chrono::task::RawTask<#impl_ty, #ret>)
             }
         }
     };
 
     let memory_type = {
-        match f.sig.output.clone() {
+        match output {
             ReturnType::Default => {
-                // quote!(Memory<F, (), *mut ::chrono::runtime::queue::Queue>)
                 quote!(Memory<F, ()>)
             }
             ReturnType::Type(_, ret) => {
-                // quote!(Memory<F, #ret, *mut ::chrono::runtime::queue::Queue>)
                 quote!(Memory<F, #ret>)
             }
         }
     };
 
     quote! {
-        #(#attrs)*
+        #inner_fn
+
         #visibility fn #fn_name(#fn_args) -> #fn_ret {
             use ::chrono::task::Memory;
 
-            #f
-
             type F = #impl_ty;
 
+            fn launder_tait(task: #fn_ret) -> #fn_ret {
+                task
+            }
+
             static MEMORY: #memory_type = Memory::alloc();
-            ::chrono::task::RawTask::new(&MEMORY, move || task(#arg_names))
+            launder_tait(::chrono::task::RawTask::new(&MEMORY, move || #inner_fn_name(#(#arg_names,)*)))
         }
     }
     .into()
