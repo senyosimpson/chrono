1ed14363c94050d773e0c09a3f3be6506ba686e2
SenYeezus <simpsonsenyo@gmail.com>
2022-11-04 08:15:06 +0200

    use chrono::init function to setup devices

--- chrono-macros/src/main.rs
+++ chrono-macros/src/main.rs
@@ -4,41 +4,11 @@ use quote::quote;
 pub(super) fn main(f: syn::ItemFn) -> TokenStream {
     let fn_body = f.block;
 
-    let hal_setup = quote! {
-        use ::chrono::hal::prelude::*;
-        use ::chrono::time;
-        let peripherals = ::chrono::hal::pac::Peripherals::steal();
-
-        // This is a workaround, so that the debugger will not disconnect immediately on asm::wfe();
-        // https://github.com/probe-rs/probe-rs/issues/350#issuecomment-740550519
-        peripherals.DBGMCU.cr.modify(|_, w| {
-            w.dbg_sleep().set_bit();
-            w.dbg_standby().set_bit();
-            w.dbg_stop().set_bit()
-        });
-
-
-        let mut rcc = peripherals.RCC.constrain();
-        let cfg = rcc.cfgr.hclk(1.MHz());
-        let mut flash = peripherals.FLASH.constrain();
-        let clocks = cfg.freeze(&mut flash.acr);
-
-        // Setup mono timer. Copied from MonoTimer::new() in stm32 hal crate
-        let mut core_peripherals = ::chrono::hal::pac::CorePeripherals::steal();
-        core_peripherals.DCB.enable_trace();
-        core_peripherals.DWT.enable_cycle_counter();
-        drop(core_peripherals.DWT);
-
-        // init time driver
-        time::driver().init(peripherals.TIM2, clocks, &mut rcc.apb1);
-    };
-
     quote! {
         async fn fut() #fn_body
 
         #[cortex_m_rt::entry]
         unsafe fn main() -> ! {
-            // #hal_setup
             ::chrono::init();
 
             static mut RT: ::chrono::Runtime = ::chrono::Runtime::new();
