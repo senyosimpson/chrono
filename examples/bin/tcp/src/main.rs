d26af38e4035dfe74f7669504d27d09116848df3
SenYeezus <simpsonsenyo@gmail.com>
2022-11-19 15:57:15 +0200

    rewrite tcp example to use multi same future feature

--- examples/bin/tcp/src/main.rs
+++ examples/bin/tcp/src/main.rs
@@ -14,8 +14,8 @@ async fn netd() {
     chrono::net::stack().start().await
 }
 
-#[chrono::alloc]
-async fn conn1() {
+#[chrono::alloc(size = 3)]
+async fn handle_tcp_conn() {
     loop {
         let (mut tx_buffer, mut rx_buffer) = chrono::net::buffer::<1024>();
         let mut socket = TcpSocket::new(&mut tx_buffer, &mut rx_buffer);
@@ -41,71 +41,13 @@ async fn conn1() {
             defmt::debug!("Message: {}", output);
         }
     }
-}
-
-#[chrono::alloc]
-async fn conn2() {
-    loop {
-        let (mut tx_buffer, mut rx_buffer) = chrono::net::buffer::<1024>();
-        let mut socket = TcpSocket::new(&mut tx_buffer, &mut rx_buffer);
-
-        socket
-            .listen(7777)
-            .expect("Failed to listen");
-
-        socket
-            .accept()
-            .await
-            .expect("Failed to accept connection");
-
-        loop {
-            let mut buf = [0; 1024];
-            match socket.read(&mut buf).await {
-                Ok(0) => break,
-                Ok(n) => defmt::debug!("Read {} bytes", n),
-                Err(e) => panic!("Read error: {}", e),
-            }
-
-            let output = core::str::from_utf8(&buf).unwrap();
-            defmt::debug!("Message: {}", output);
-        }
-    }
-}
-
-#[chrono::alloc]
-async fn conn3() {
-    loop {
-        let (mut tx_buffer, mut rx_buffer) = chrono::net::buffer::<1024>();
-        let mut socket = TcpSocket::new(&mut tx_buffer, &mut rx_buffer);
-
-        socket
-            .listen(7777)
-            .expect("Failed to listen");
-
-        socket
-            .accept()
-            .await
-            .expect("Failed to accept connection");
-
-        loop {
-            let mut buf = [0; 1024];
-            match socket.read(&mut buf).await {
-                Ok(0) => break,
-                Ok(n) => defmt::debug!("Read {} bytes", n),
-                Err(e) => panic!("Read error: {}", e),
-            }
-
-            let output = core::str::from_utf8(&buf).unwrap();
-            defmt::debug!("Message: {}", output);
-        }
-    }
-
 }
 
 #[chrono::main]
 async fn main() -> ! {
     let _ = chrono::spawn(netd()).expect("Could not spawn net daemon");
-    let _ = chrono::spawn(conn1()).expect("Could not spawn conn1");
-    let _ = chrono::spawn(conn2()).expect("Could not spawn conn2");
-    chrono::spawn(conn3()).expect("Could not spawn conn3").await;
+    let _ = chrono::spawn(handle_tcp_conn()).expect("Could not spawn task 1");
+    let _  = chrono::spawn(handle_tcp_conn()).expect("Could not spawn task 2");
+
+    chrono::spawn(handle_tcp_conn()).expect("Could not spawn task 3").await;
 }
